c05d1cfd73e47af99c6fdedda28a3331970c3471
Andy Fraley <andrew.fraley@gmail.com>
2014-07-30 10:16:31 -0400

    updated metadata for supermarket

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'MIT'
 description      'Installs/Configures rhn-channels'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports 'redhat', ">= 6.0"
-version          '1.1.0'
+version          '1.1.1'
