5e572f6fad33c55b579465fc02fdeaa3932a1f27
Andy Fraley <andrew.fraley@gmail.com>
2014-07-30 09:36:59 -0400

    added supports line to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,5 @@ maintainer_email 'andrew.fraley@gmail.com'
 license          'MIT'
 description      'Installs/Configures rhn-channels'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+supports 'redhat', ">= 6.0"
 version          '1.1.0'
