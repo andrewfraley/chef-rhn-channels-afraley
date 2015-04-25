bdbf7c5ea76e0978e28ab973e446184a8fc3b4f6
John Bellone <john.bellone.jr@gmail.com>
2015-04-25 18:25:38 -0400

    Update the metadata.rb file with issues/source URL.

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
 name 'rhn-channels'
-maintainer 'Andy Fraley'
-maintainer_email 'andrew.fraley@gmail.com'
+maintainer 'John Bellone'
+maintainer_email 'jbellone@bloomberg.net'
 license 'MIT'
 description 'Manages channel subscriptions to RHN satellite network.'
 long_description 'Manages channel subscriptions to RHN satellite network.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
+source_url 'https://github.com/johnbellone/rhn-channels-cookbook'
+issues_url 'https://github.com/johnbellone/rhn-channels-cookbook/issues'
 
 supports 'redhat', '>= 5.0'
 
