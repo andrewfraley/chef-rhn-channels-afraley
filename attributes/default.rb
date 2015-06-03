6563bea761a6877199bae82b1f5ac316eb9a3faf
John Bellone <jbellone@bloomberg.net>
2015-06-03 10:10:04 -0400

    Adds guard clause to only default attributes on RHEL.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,15 +5,16 @@
 # Copyright 2014, 2015 Andy Fraley
 # Copyright 2015 Bloomberg Finance L.P.
 #
-
-case node['platform_version'].to_i
-when 5
-  default['rhn-channels']['optional'] = nil
-  default['rhn-channels']['supplementary'] = 'rhel-x86_64-server-supplementary-5'
-when 6
-  default['rhn-channels']['optional'] = 'rhel-x86_64-server-optional-6'
-  default['rhn-channels']['supplementary'] = 'rhel-x86_64-server-supplementary-6'
-when 7
-  default['rhn-channels']['optional'] = 'rhel-x86_64-server-optional-7'
-  default['rhn-channels']['supplementary'] = 'rhel-x86_64-server-supplementary-7'
+if node['platform'] == 'redhat'
+  case node['platform_version'].to_i
+  when 5
+    default['rhn-channels']['optional'] = nil
+    default['rhn-channels']['supplementary'] = 'rhel-x86_64-server-supplementary-5'
+  when 6
+    default['rhn-channels']['optional'] = 'rhel-x86_64-server-optional-6'
+    default['rhn-channels']['supplementary'] = 'rhel-x86_64-server-supplementary-6'
+  when 7
+    default['rhn-channels']['optional'] = 'rhel-x86_64-server-optional-7'
+    default['rhn-channels']['supplementary'] = 'rhel-x86_64-server-supplementary-7'
+  end
 end
